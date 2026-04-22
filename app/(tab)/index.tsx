0fb0edf6b2434f192025b25833a4d058c4c842c0
jatin-pandey01 <=>
2026-04-23 00:12:45 +0530

    Modal fixed

--- app/(tab)/index.tsx
+++ app/(tab)/index.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import {
+  Modal,
   ScrollView,
   StyleSheet,
   Text,
+  TextInput,
   TouchableOpacity,
   View,
 } from "react-native";
@@ -10,6 +12,10 @@ import VehicleCard from "../../component/VehicleCard";
 
 const Home = () => {
   const [vehicles, setVehicles] = useState<any[]>([]);
+
+  const [modalVisible, setModalVisible] = useState(false);
+  const [name, setName] = useState("");
+  const [type, setType] = useState("");
 
   useEffect(() => {
     fetch("https://vehicle-management-1-vz7s.onrender.com/vehicles")
@@ -22,6 +28,36 @@ const Home = () => {
       .catch((error) => console.log(`Error :: ${error}`));
   }, []);
 
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch(
+        "https://vehicle-management-1-vz7s.onrender.com/vehicles",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name,
+            type,
+          }),
+        },
+      );
+
+      const data = await response.json();
+
+      // Update UI instantly
+      setVehicles((prev) => [data, ...prev]);
+
+      // Reset + close modal
+      setName("");
+      setType("");
+      setModalVisible(false);
+    } catch (error) {
+      console.log("Submit Error:", error);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <ScrollView>
@@ -33,10 +69,45 @@ const Home = () => {
       {/* Floating Button */}
       <TouchableOpacity
         style={styles.fab}
-        onPress={() => console.log("Add Vehicle")}
+        onPress={() => setModalVisible(true)}
       >
         <Text style={styles.fabText}>+</Text>
       </TouchableOpacity>
+
+      <Modal visible={modalVisible} animationType="fade" transparent={true}>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.title}>Add Vehicle</Text>
+
+            <TextInput
+              placeholder="Vehicle Name"
+              value={name}
+              onChangeText={setName}
+              style={styles.input}
+            />
+
+            <TextInput
+              placeholder="Vehicle Type"
+              value={type}
+              onChangeText={setType}
+              style={styles.input}
+            />
+
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity style={styles.submitBtn} onPress={handleSubmit}>
+                <Text style={styles.btnText}>Submit</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={styles.cancelBtn}
+                onPress={() => setModalVisible(false)}
+              >
+                <Text style={styles.btnText}>Cancel</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -70,6 +141,63 @@ const styles = StyleSheet.create({
   fabText: {
     color: "#fff",
     fontSize: 28,
+    fontWeight: "bold",
+  },
+
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+
+  modalContent: {
+    width: "80%",
+    backgroundColor: "#fff",
+    padding: 20,
+    borderRadius: 10,
+  },
+
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 5,
+  },
+
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between", // or "center"
+    marginTop: 10,
+  },
+
+  submitBtn: {
+    backgroundColor: "#007AFF",
+    padding: 12,
+    borderRadius: 5,
+    flex: 1,
+    marginRight: 5,
+    alignItems: "center",
+  },
+
+  cancelBtn: {
+    backgroundColor: "#ccc",
+    padding: 12,
+    borderRadius: 5,
+    flex: 1,
+    marginLeft: 5,
+    alignItems: "center",
+  },
+
+  btnText: {
+    color: "#fff",
     fontWeight: "bold",
   },
 });
